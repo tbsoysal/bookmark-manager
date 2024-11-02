a774a5f9dcb987d293670cc14297f236262c295c
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-11-02 19:06:27 +0300

    add reset button and localstorage feature

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const urlInput = document.querySelector("#url");
-const addBtn = document.querySelector(".urlbox button");
+const resetBtn = document.querySelector("#resetBtn");
 const form = document.querySelector("form");
 const categoryEl = document.querySelector("select");
 const nameEl = document.querySelector("form>input");
@@ -8,63 +8,94 @@ const selfimproveList = document.querySelector(".bookmarks-selfimprovement");
 const visitlaterList = document.querySelector(".bookmarks-visitlater");
 const favoritesList = document.querySelector(".bookmarks-favorites");
 
-const educationLinks = [];
-const selfimprovementLinks = [];
-const visitlaterLinks = [];
-const favoritesLinks = [];
+let educationLinks = [];
+let selfimprovementLinks = [];
+let visitlaterLinks = [];
+let favoritesLinks = [];
+
+window.onload = getLocalStorageData;
+
+resetBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  educationLinks = [];
+  selfimprovementLinks = [];
+  visitlaterLinks = [];
+  favoritesLinks = [];
+  localStorage.clear();
+  displayLinks();
+})
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const newBookMark = {
     name: nameEl.value,
     category: categoryEl.value,
-    url: urlInput.value
+    url: urlInput.value,
   };
 
   switch (newBookMark.category) {
     case "favorites":
-        favoritesLinks.push(newBookMark);
+      favoritesLinks.push(newBookMark);
       break;
     case "visitlater":
-        visitlaterLinks.push(newBookMark);
+      visitlaterLinks.push(newBookMark);
       break;
     case "selfimprovement":
-        selfimprovementLinks.push(newBookMark);
+      selfimprovementLinks.push(newBookMark);
       break;
     case "education":
-        educationLinks.push(newBookMark);
+      educationLinks.push(newBookMark);
       break;
   }
 
   displayLinks();
-})
+  updateLocalStorageData();
+});
 
+function updateLocalStorageData() {
+  const currEducation = JSON.stringify(educationLinks);
+  const currSelf = JSON.stringify(selfimprovementLinks);
+  const currVisit = JSON.stringify(visitlaterLinks);
+  const currFav = JSON.stringify(favoritesLinks);
+
+  localStorage.setItem("education", currEducation);
+  localStorage.setItem("selfimprovement", currSelf);
+  localStorage.setItem("visitlater", currVisit);
+  localStorage.setItem("currFav", currFav);
+}
+
+function getLocalStorageData() {
+  if (localStorage.length) {
+    educationLinks = JSON.parse(localStorage.getItem("education"));
+    selfimprovementLinks = JSON.parse(localStorage.getItem("selfimprovement"));
+    visitlaterLinks = JSON.parse(localStorage.getItem("visitlater"));
+    favoritesLinks = JSON.parse(localStorage.getItem("currFav"));
+  }
+  displayLinks();
+}
 
 function displayLinks() {
   favoritesList.innerHTML = "";
-  favoritesLinks.forEach(link => {
+  favoritesLinks.forEach((link) => {
     const bookmarkEl = `<a href="${link.url}">${link.name}</a>`;
     favoritesList.innerHTML += bookmarkEl;
-  })
+  });
 
   visitlaterList.innerHTML = "";
-  visitlaterLinks.forEach(link => {
+  visitlaterLinks.forEach((link) => {
     const bookmarkEl = `<a href="${link.url}">${link.name}</a>`;
     visitlaterList.innerHTML += bookmarkEl;
-  })
+  });
 
   selfimproveList.innerHTML = "";
-  selfimprovementLinks.forEach(link => {
+  selfimprovementLinks.forEach((link) => {
     const bookmarkEl = `<a href="${link.url}">${link.name}</a>`;
     selfimproveList.innerHTML += bookmarkEl;
-  })
+  });
 
   educationList.innerHTML = "";
-  educationLinks.forEach(link => {
+  educationLinks.forEach((link) => {
     const bookmarkEl = `<a href="${link.url}">${link.name}</a>`;
     educationList.innerHTML += bookmarkEl;
-  })
+  });
 }
-
-
-
